06e804fb9a86f78c72835a15272549613acd7612
Sean Taylor <seankimbletaylor@gmail.com>
2024-03-02 23:11:37 -0800

    Add spider blog post

---
+++ docs/_site/assets/js/2024-03-02-spider.js
@@ -0,0 +1,5 @@
+import { BasicThreeJs, COLORS } from "./threejs-basic.js";
+
+new BasicThreeJs(".threejs.chassis", "spider/chassis", COLORS.white);
+new BasicThreeJs(".threejs.legHousing", "spider/legHousing", COLORS.green);
+new BasicThreeJs(".threejs.legExtension", "spider/legExtension", COLORS.red);
